2f698d89b2be4d3c6972428e0918553eac1a6383
Irene Arsuaga <irenearsu@gmail.com>
2018-01-15 18:29:51 +0100

    Implementada la accion de sacar fotos

--- APPrendeus/app/src/main/java/eus/ehu/adibidea/tta/apprendeus/LoginActivity.java
+++ APPrendeus/app/src/main/java/eus/ehu/adibidea/tta/apprendeus/LoginActivity.java
@@ -1,11 +1,25 @@
 package eus.ehu.adibidea.tta.apprendeus;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Uri pictureUri;
+    public static final int PICTURE_REQUEST_CODE = 0;
+    public static final int READ_REQUEST_CODE = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +30,46 @@ public class LoginActivity extends AppCompatActivity {
     public void login(View view){
         Intent intent = new Intent(this,Menu1Activity.class);
         startActivity(intent);
+    }
+
+    public void argazkia(View view){
+        if(!getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA))
+            Toast.makeText(this,R.string.no_camera,Toast.LENGTH_SHORT).show();
+        else{
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if(intent.resolveActivity(getPackageManager())!=null){
+                File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+                try{
+                    File file = File.createTempFile("tta",".jpg",dir);
+                    pictureUri = Uri.fromFile(file);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT,pictureUri);
+                    startActivityForResult(intent,PICTURE_REQUEST_CODE);
+                }
+                catch(IOException e){
+
+                }
+            }
+            else
+                Toast.makeText(this,R.string.no_camera,Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void sendFile(Uri uri){
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("*/*");
+        startActivityForResult(intent,READ_REQUEST_CODE);
+
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(resultCode != Activity.RESULT_OK)
+            return;
+        switch (requestCode){
+            case PICTURE_REQUEST_CODE:
+                sendFile(pictureUri);
+                break;
+        }
     }
 }
